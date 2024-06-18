e381c4f4ba6b64793fcfb72592afd6e4020326ae
Wilmer Oidor <wilmer.oidor@globant.com>
2024-06-18 17:24:24 -0500

    adding lint files

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+
+
+export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.jsx"], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  ...fixupConfigRules(pluginReactConfig),
+];
