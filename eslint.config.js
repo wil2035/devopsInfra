5ce7e270bf2e9d71029b82f6dc3d4cac34bf011d
Wilmer Oidor <wilmer.oidor@globant.com>
2024-06-18 22:38:15 -0500

    ***

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,9 @@ import globals from "globals";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { languageOptions: { globals: globals.browser } },
   { files: ["**/*.jsx"], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   ...fixupConfigRules(pluginReactConfig),
 ];
